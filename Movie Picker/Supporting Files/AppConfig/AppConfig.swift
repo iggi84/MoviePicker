7f97b4bc21ea0b70e4df9e7d63bf5d5ff4c55aeb
Igor Vojinovic <igor.vojinovic@optmia-apps.com>
2024-03-11 22:03:02 +1100

    Prepared various configurations for production and stage. Added separated configurations.

---
+++ Movie Picker/Supporting Files/AppConfig/AppConfig.swift
@@ -0,0 +1,31 @@
+//
+//  AppConfig.swift
+//  Movie Picker
+//
+//  Created by Igor  Vojinovic on 11.3.24..
+//
+
+import UIKit
+
+extension UIApplication {
+    
+    // MARK: - Properties
+    
+    class var config: NSDictionary {
+        return Bundle.main.object(forInfoDictionaryKey: "Config") as! NSDictionary
+    }
+    
+    @objc class var baseURL: String {
+        guard let baseURL = config["BaseURL"] as? String else {
+            return ""
+        }
+        return baseURL
+    }
+    
+    @objc class var apiToken: String {
+        guard let apiToken = config["APIToken"] as? String else {
+            return ""
+        }
+        return apiToken
+    }
+}

84097418d737fd26492b26d5a0adbc15118bd835
Igor Vojinovic <igor.vojinovic@optmia-apps.com>
2024-03-11 16:30:11 +1100

    Created project base, with subfolders structure.

---
+++ Movie Picker/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Picker
+//
+//  Created by Igor  Vojinovic on 11.3.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
